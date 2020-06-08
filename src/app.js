6893a7cdf65773c5eee05225f2ad72c8b4e79d24
Chethan K <krishnamurthy.chethan@gmail.com>
2020-06-08 23:02:29 +0530

    setting up routes using react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,14 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {BrowserRouter, Route} from "react-router-dom";
 import ReactModal from "react-modal";
 
 import "normalize.css/normalize.css";
 import "./styles/app.scss";
 
+const ExpenseDashboard = () => (
+    <div>
+        <h1>Expense Dashboard</h1>
+    </div>
+);
 
-console.log('no live server');
-ReactDOM.render(<p>expensify</p>, document.getElementById("app"));
+const AddExpense = () => (
+    <div>
+        <h1>Add Expense</h1>
+    </div>
+);
+
+const EditExpense = () => (
+    <div>
+        <h1>Edit Expense</h1>
+    </div>
+);
+
+const Help = () => (
+    <div>
+        <h1>HELP</h1>
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <Route exact={true} path="/" component={ExpenseDashboard} />
+        <Route path="/add" component={AddExpense} />
+        <Route path="/edit" component={EditExpense} />
+        <Route path="/help" component={Help} />
+    </BrowserRouter>
+);
+
+
+ReactDOM.render(routes, document.getElementById("app"));
 
 // To handle warning thrown by react-modal
 ReactModal.setAppElement(document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
     },
     devtool: "cheap-module-eval-source-map",
     devServer: {
-        contentBase: outputPath
-    }
+        contentBase: outputPath,
+        historyApiFallback: true
+    },
 };
