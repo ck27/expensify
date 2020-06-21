41b93e7a2dace05663479a31bf3886042b60a895
Chethan K <krishnamurthy.chethan@gmail.com>
2020-06-21 18:14:47 +0530

    Redux with combined reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -27,7 +27,19 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
                 action.expense
             ];
         case "DELETE_EXPENSE":
-            return state.filter ( ( {id} ) => id !== action.expenseId );
+            return state.filter( ( {id} ) => id !== action.expenseId );
+
+        case "EDIT_EXPENSE":
+            return state.map( (exp) => {
+                if(exp.id == action.expense.id) {
+                    return {
+                        ...exp,
+                        ...action.expense
+                    };
+                } else {
+                    return exp;
+                }
+            });
         default:
             return state;
     }
@@ -43,7 +55,33 @@ let filtersReducerDefaultState = {
 };
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
-    switch(action) {
+    switch(action.type) {
+        case "TEXT_FILTER":
+            return {
+                ...state,
+                text : action.text
+            };
+        case "SORT_BY_DATE":
+            return {
+                ...state,
+                sortBy: "date"
+            };
+        case "SORT_BY_AMOUNT":
+            return {
+                ...state,
+                sortBy: "amount"
+            };
+        case "SET_START_DATE":
+            return {
+                ...state,
+                startDate : action.startDate
+            };
+        case "SET_END_DATE":
+            return {
+                ...state,
+                endDate: action.endDate
+            }
+
         default:
             return state;
     }
@@ -66,6 +104,36 @@ const deleteExpense = ( expenseId ) => ( {
     expenseId
 });
 
+const editExpense = ( expense ) => ({
+    type: "EDIT_EXPENSE",
+    expense
+});
+
+const setTextFilter = ( text = "" ) => ({
+    type: "TEXT_FILTER",
+    text
+});
+
+const sortByAmount = () => ({
+    type: "SORT_BY_AMOUNT"
+});
+
+const sortByDate = () => ({
+    type: "SORT_BY_DATE"
+});
+
+const setStartDate = (startDate) => ({
+    type: "SET_START_DATE",
+    startDate
+
+});
+
+const setEndDate = (endDate) => ({
+    type: "SET_END_DATE",
+    endDate
+
+});
+
 const expensesStore = createStore(
     combineReducers({
         expenses: expensesReducer,
@@ -73,12 +141,47 @@ const expensesStore = createStore(
     })
 );
 
+const getVisibleExpenses = (expenses, {text, startDate, endDate, sortBy}) => {
+    return expenses.filter( (expense) => {
+        const textMatch = !!expense.description && expense.description.toLowerCase().includes(text.toLowerCase());
+        const startDateMatch = typeof(startDate) !== 'number' || expense.createdAt >= startDate  
+        const endDateMatch = typeof(endDate) !== 'number' || expense.createdAt <= endDate  
+        return textMatch && startDateMatch && endDateMatch;
+    }).sort( (a, b) => {
+
+        if(sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1
+        }else if(sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
+    });
+}
+
 expensesStore.subscribe( () => {
-    console.log(expensesStore.getState());
+    const state = expensesStore.getState();
+    const expenses = getVisibleExpenses(state.expenses, state.filter);
+    console.log(expenses);
 });
 
 
-const exp1 = expensesStore.dispatch( addExpense( { amount : 1000, description : "rent june 2020" }));
-const exp2 = expensesStore.dispatch( addExpense( { amount : 500, description : "Shopping June 2020" }));
+const exp1 = expensesStore.dispatch( addExpense( { amount : 1000, description : "rent june 2020", createdAt:2020 }));
+const exp2 = expensesStore.dispatch( addExpense( { amount : 500, description : "Shopping June 2020", createdAt: -1989 }));
+const exp3 = expensesStore.dispatch( addExpense( { amount : 500, description : "Fuel", createdAt: 2000 }));
 
-expensesStore.dispatch( deleteExpense( exp1.expense.id));
+// expensesStore.dispatch( deleteExpense( exp1.expense.id));
+// expensesStore.dispatch( setTextFilter("sho"));
+
+// const exp3 = expensesStore.dispatch( editExpense ( { 
+//     ...exp2.expense,
+//     amount: 800
+// } ) );
+
+// expensesStore.dispatch( setTextFilter());
+
+expensesStore.dispatch( sortByAmount())
+
+// expensesStore.dispatch( sortByDate());
+
+// expensesStore.dispatch( setStartDate(2000) );
+// expensesStore.dispatch( setStartDate() );
+// expensesStore.dispatch( setEndDate(2020) );
