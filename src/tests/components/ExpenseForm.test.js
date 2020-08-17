7b0b5e132fe36b75e49166d00d2dd7d6b78e044f
Chethan K <krishnamurthy.chethan@gmail.com>
2020-08-17 23:56:11 +0530

    Testing user interactions

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -13,3 +13,51 @@ test("it must render expense form when data is passed", () =>{
     const content = shallow(<ExpenseForm  expense={anExpense} />);
     expect(content).toMatchSnapshot();
 });
+
+test("form submission without mandatory fields will show error message", () => {
+    const content = shallow(<ExpenseForm />);
+    expect(content).toMatchSnapshot();
+    content.find("form").simulate("submit", { preventDefault: () => {} });
+    expect(content.state("isValid")).toBeFalsy();
+    expect(content).toMatchSnapshot();
+});
+
+test("should update state.description on input change", () => {
+    const value = "Coffee";
+    const content = shallow(<ExpenseForm />);
+    content.find("input").at(0).simulate("change", {
+        target : { value }
+    });
+    expect(content.state("description")).toBe(value);
+    expect(content).toMatchSnapshot();
+});
+
+test("should update state.note on input change", () => {
+    const value = "Coffee with friends";
+    const content = shallow(<ExpenseForm />);
+    content.find("textarea").simulate("change", {
+        target : {value}
+    });
+    expect(content.state("note")).toBe(value);
+    expect(content).toMatchSnapshot();
+})
+
+test("should update state.amount on valid input", () => {
+    const value = "200.00";
+    const content = shallow(<ExpenseForm />);
+    content.find("input").at(1).simulate("change", {
+        target : {value}
+    });
+    expect(content.state("amount")).toBe(value);
+    expect(content).toMatchSnapshot();
+});
+
+test("should not set state.amount on invalid input", () => {
+    const value = "200.999";
+    const content = shallow(<ExpenseForm />);
+    content.find("input").at(1).simulate("change", {
+        target : {value}
+    });
+    expect(content.state("amount")).toBe("");
+    expect(content).toMatchSnapshot();
+})
